3819f54f4965f1386f20710c557f4766d480f1f4
K <chenkaiwen1997@gmail.com>
2021-01-12 05:44:39 +0800

    finish

--- flaskr/static/utils.js
+++ flaskr/static/utils.js
@@ -1,21 +1,39 @@
-function queries_per_filter_predicates(videoMode, inputVideo, action, classes) {
-    var query;
+function camelize(str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
+      return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    }).replace(/\s+/g, '');
+  }
+function setUpIndicator(accumulator, currentValue){
+    const cur = camelize(currentValue)
+    const i = accumulator[1];
+    return [accumulator[0] + `, I${i}(${cur}Detections) AS ${cur}Indicator`, accumulator[1] + 1]
+}
 
-    if (classes.length == 0){
-        query = "SELECT cameraID, frameID\nFROM (PROCESS "
-     + inputVideo + 
-     " PRODUCE cameraID, frameID USING " + videoMode + " mode)\nWHERE action = " + action;
-  
+function setUpDetections(acc,cur){
+    return acc + `, ${camelize(cur)}Detections`;
+}
+function setUpWhere(acc,cur){
+    return acc + `, ${camelize(cur)}Indicator == 1`
+}
+
+function queries_per_filter_predicates(videoMode, inputVideo, action, classes) {
+    action = camelize(action)
+    var query;
+    if(classes.length == 0){
+        query = `SELECT  MERGE(clipID) AS Sequence, I1(${action}Detections) AS ${action}Indicator\nFROM (PROCESS ${inputVideo}` +
+       ` PRODUCE clipID, ${action}Detections USING ActionClassifier) \nWhere ${action}Indicator == 1`
     }
     else{
-        query = "SELECT cameraID, frameID\nFROM (PROCESS "
-        + inputVideo + 
-        " PRODUCE cameraID, frameID USING " + videoMode + " mode)\nWHERE action = " + action + " AND class = " + classes;
-
+        query = `SELECT  MERGE(clipID) AS Sequence, I1(${action}Detections) AS ${action}Indicator${classes.reduce(setUpIndicator,["",2])[0]}\nFROM (PROCESS ${inputVideo}` +
+        ` PRODUCE clipID${classes.reduce(setUpDetections,"")} USING ObjectDetector, ${action}Detections USING ActionClassifier) \nWhere ${action}Indicator == 1${classes.reduce(setUpWhere,"")}`
     }
   
     return query;
 }
+
+
+// SELECT  MERGE(clipID) AS Sequence , I1(humanDetections) AShumanIndicator , I2(carDetections) AS  carIndicator , I3(jumpingDetections) AS  jumpingIndicator ,
+// FROM (PROCESS  inputVideo  PRODUCE  clipID , humanDetections ,carDetections  USING  ObjectDetector , jumpingDetections  USINGActionClassifier)WHERE  humanIndicator =1 AND  carIndicator =1 AND  jumpingIndicator =1
 
 function offline_sql(inputVideo, action, classes,k) {
     var query;
